3cddd6a207f80bc199418c74dc2ad2715b63b756
hilarywenyi <hilarywenyi@gmail.com>
2019-01-27 16:24:45 -0500

    feat: simple login form using React Hooks

--- app/index.js
+++ app/index.js
@@ -1,27 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import useInputState from './useInputState';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
 
-const Form = ({state}) => {
-    const { value, reset, onChange } = useInputState('');
-    return (
-        <div id='container'>
-          <div id='navbar'>
-            Demo Form Using React Hooks
+const Form = () => {
+
+      const [value, setValue] = useState({username: '', password: ''})
+      return (
+          <div id='container'>
+            <div id='navbar'>
+              Demo Form Using React Hooks
+            </div>
+            <form onSumbit={event => {
+                event.preventDefault();
+             }}>
+               <label htmlFor ='username'>Username:</label>
+               <input
+                type='text'
+                name = 'username'
+                onChange = {event=>{
+                  setValue(event.target.value)}}
+                value = {value.username}
+               />
+
+               <label htmlFor ='password'>Password:</label>
+               <input
+                type='text'
+                name = 'password'
+                onChange = {event=>{
+                  setValue(event.target.value)}}
+                value = {value.passwords}
+               />
+               <button type = 'submit'>Submit</button>
+            </form>
           </div>
-          <form onSumbit={event => {
-              event.preventDefault();
-              state(value); 
-              reset();
-           }}>
-             <label htmlFor ='username'>Username:</label>
-             <input type='text' name = 'username' onChange = {onChange} value = {value}/>
-             <label htmlFor = 'password'>Password:</label>
-             <input type = 'password' name = 'password' onChange = {onChange} value = {value} />
-             <button type = 'submit'>Submit</button>
-          </form>
-        </div>
-      )
+        )
 }
 
 ReactDOM.render(
